be1ba6024e6ea0246ea4814598aa9f5d9964c3e2
Matheus Diniz <71847134+MatheusDiniz-ES@users.noreply.github.com>
2021-10-28 17:37:29 -0300

    Add files via upload

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,8 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 
 const port = process.env.PORT || 3000;
 
